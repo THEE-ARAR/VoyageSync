05d5adba710ade029694905f06c3ec8a7bf10278
regan wilson <regan1.wilson@famu.edu>
2024-05-02 17:48:20 -0400

    MESSSAGES

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -14,7 +14,7 @@ function HomePage() {
     return (
         <div>
             <header>
-                <h1>Welcome to Our Application</h1>
+                <h1>Welcome to VoyageSync</h1>
             </header>
             <nav>
                 <ul>
